ba1e7b1ce7befa486816ec09eeddf7137eb9db15
Andrew <andrey_emelyanov_2005@mail.ru>
2025-09-01 14:18:57 +0300

    for merge

--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
 
 from app.users.router import router as users_router
+from app.tables.router import router as tables_router
 
 app = FastAPI()
 
 app.include_router(users_router)
-
-
---
+++ app/tables/router.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+
+router = APIRouter(
+    prefix="/tables",
+    tags=["Бронь столов"]
+)
+
